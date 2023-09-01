9a638ad7feb86cceec8a91164d9d82e780336026
Mesrob Kyurkchyan <mesrobk.dev@gmail.com>
2023-08-31 21:39:52 -0700

    lets learn italian

---
+++ main.py
@@ -0,0 +1,54 @@
+from prompt_toolkit import prompt
+import csv
+import random
+
+phrases = []
+count = 0
+
+
+def initialze_dictionary():
+    pass
+    # read from csv file 'basic-italian-phrases.csv' and add each row to the array phrases
+
+
+def get_random_italian_phrase():
+    pass
+    # generate random index and print the row from the phrases array
+
+
+def next_phrase():
+    global count
+    # Print each row from the phrases array and increment count index
+
+
+def main():
+    print("Let's start learning Italian!")
+    initialze_dictionary()
+    while True:
+        text = prompt(
+            'Would you like random phrases or to follow a list? (random/list) ')
+        if text == "random":
+            get_random_italian_phrase()
+            while True:
+                text = prompt(
+                    'Would you like another phrase (y/n)? ')
+                if text == 'y':
+                    get_random_italian_phrase()
+                else:
+                    break
+        elif text == "list":
+            next_phrase()
+            while True:
+                text = prompt(
+                    'Would you like another phrase (y/n)? ')
+                if text == 'y':
+                    next_phrase()
+                else:
+                    break
+        else:
+            print("Ciao bellissima!")
+            break
+
+
+if __name__ == '__main__':
+    main()
